5d39a8d080b18c428b2acfa0e9939c438d63906f
Bry <134396548+brruhyan@users.noreply.github.com>
2024-11-26 15:06:40 +0800

    Create App.js

---
+++ App.js
@@ -0,0 +1,65 @@
+import React, {useState} from 'react';
+import { ScrollView, StyleSheet, View } from 'react-native';
+import NotificationContainer from './Notification';
+import Header from './Header';
+import Greetings from './Greetings';
+import Summary from './Summary';
+import ShowCameraFeed from './ShowCameraFeed';
+
+function HomeScreen() {
+
+  const [inferenceResults, setInferenceResults] = useState(null);
+
+  const updateInferenceResults = (newInferenceResults) => {
+    console.log('Received new inference results:', newInferenceResults);
+    setInferenceResults(newInferenceResults);
+  };
+
+  return (
+    <ScrollView style={styles.appContainer}>
+      <View style={styles.notificationWrapper}>
+        <NotificationContainer />
+      </View>
+
+      <Header />
+      <Greetings />
+
+      <View style={styles.summaryContainer}>
+        <Summary inferenceResults={inferenceResults}/>
+      </View>
+
+      <View style={styles.showCameraFeedContainer}>
+        <ShowCameraFeed onInferenceResults={updateInferenceResults}/>
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    backgroundColor: '#fff',
+    flex: 1,
+    paddingTop: 100, 
+  },
+
+  notificationWrapper: {
+    position: 'absolute',
+    top: 60,
+    right: 10,
+    zIndex: 10,
+  },
+
+  summaryContainer: {
+    width: '100%',
+  },
+
+  showCameraFeedContainer: {
+    flexGrow: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    marginVertical: 0,
+  },
+});
+
+export default HomeScreen;
